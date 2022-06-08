42b2f0d0af841b107a5ead0948e308fd107049d0
onlyTT5 <2218201514@qq.com>
2022-06-08 15:53:04 +0800

    commit codes file

---
+++ codes/光天化日学C语言/20-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int a = 0;
+    a = a + 1.5;
+    printf("%d\n", a);
+    return 0;
+}
---
+++ codes/光天化日学C语言/20-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int a, b, c, d = 0;
+    a = (b = (c = (d = (d == 0))));
+    printf("%d\n", a);
+    return 0;
+}
